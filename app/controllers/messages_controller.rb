cc0cc55f836966b95ded546859b3166e9db1701b
Jerrold Thompson <jet@whidbey.com>
2017-02-02 16:35:13 -0800

    crud for messages and views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,9 @@
 class MessagesController < ApplicationController
+
+  before_action :find_message, only: [:show, :edit, :update, :destroy]
+
   def index
+    @messages = Message.all.order(created_at: :desc)
   end
 
   def new
@@ -8,10 +12,39 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    if @message.save
+      redirect_to @message
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to @message
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path
   end
 
   private
 
   def message_params
     params.require(:message).permit(:title, :content)
+  end
+
+  def find_message
+    @message = Message.find(params[:id])
+  end
 end
