df1eca19bff6aa5ac96a426ae3e094fc1da6ca71
Jerrold Thompson <jet@whidbey.com>
2017-02-12 11:32:26 -0800

    migrate comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203122301) do
+ActiveRecord::Schema.define(version: 20170212192658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "message_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["message_id"], name: "index_comments_on_message_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "messages", force: :cascade do |t|
     t.string   "title"
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 20170203122301) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "messages"
+  add_foreign_key "comments", "users"
 end
