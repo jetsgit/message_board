2f6d1cb9bee886d22519b5b08d8d251eef2474f5
Jerrold Thompson <jet@whidbey.com>
2017-02-21 15:20:45 -0800

    feature spec for clearing new message form

--- spec/features/new-message_spec.rb
+++ spec/features/new-message_spec.rb
@@ -22,7 +22,7 @@ feature "signed in user creates message" do
     click_link 'New Msg'
     fill_in 'message[content]', with: msg.content
     click_button 'Create Message'
-    # there is really an asterisk in below string, which is causing some silliness
+    #  some silliness below
     expect(page).to have_content("Titlecan't be blank")
   end
 
@@ -34,8 +34,22 @@ feature "signed in user creates message" do
     click_link 'New Msg'
     fill_in 'message[title]', with: msg.title
     click_button 'Create Message'
-    # there is really an asterisk in below string, which is causing some silliness
     expect(page).to have_content("Content can't be blank")
+  end
+
+  scenario 'message form is cleared when close button is clicked' do
+    msg  = build(:message, @current_user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in_signin_fields
+    click_link 'New Msg'
+    fill_in 'message[title]', with: msg.title
+    click_button 'Create Message'
+    expect(page).to have_content("Content can't be blank")
+    first(:xpath, '//button[@id="close-message"]').click
+    visit root_path
+    first(:xpath, '//a[contains(text(), "New Msg")]').click
+    expect(page).to_not have_content("Content can't be blank")
   end
 
 end
