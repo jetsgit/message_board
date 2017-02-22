103813e3263b8e268cfae10645d25625b2d82596
Jerrold Thompson <jet@whidbey.com>
2017-02-22 15:53:03 -0800

    spec for comment#create

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  login_user
+  before(:each) do
+    @message = FactoryGirl.create :message
+  end
+
+  describe 'POST #create' do
+    it 'saves a new comment to the database' do
+      @comment_attributes = FactoryGirl.attributes_for(:comment, message_id: @message.id)
+      expect {
+        post :create, message_id: @message, comment: @comment_attributes 
+      }.to change(Comment, :count).by(1)
+    end
+  end
 
 end
