192c83a1f99a096b9634a2202334f1dce1dcf7fd
Jerrold Thompson <jet@whidbey.com>
2017-03-02 18:21:21 -0800

    Revert "implement will_paginate"
    This reverts commit 73ff2ceabc93d52f851f6ef229b7ba68f11ca02f.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+  # validates :title, presence: true
+  # validates :content, presence: true
   validates_presence_of :title, :content
-  self.per_page = 10
 end
