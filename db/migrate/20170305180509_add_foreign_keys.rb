b2a58ef54c319e72d2f3f47fc40f81596f23160b
Jerrold Thompson <jet@whidbey.com>
2017-03-05 10:10:38 -0800

    alter migration to cascade delete in db

--- db/migrate/20170305180509_add_foreign_keys.rb
+++ db/migrate/20170305180509_add_foreign_keys.rb
@@ -1,5 +1,5 @@
 class AddForeignKeys < ActiveRecord::Migration
   def change
-    add_foreign_key "messages", "users", name: "messages_user_id_fk"
+    add_foreign_key "messages", "users", name: "messages_user_id_fk", dependent: :delete
   end
 end
