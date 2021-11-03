e3d63da064493907dc5f0a95fe4941cee7875c93
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-03 18:12:13 +0800

    Delete app directory

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  belongs_to :message
-  belongs_to :user
-  validates_presence_of :comment 
-end
--- app/models/message.rb
+++
@@ -1,6 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  validates_presence_of :title, :content
-  self.per_page = 16
-end
