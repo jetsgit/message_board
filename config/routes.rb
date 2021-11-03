a5b328b7120a26bc16f74a60e6e60842139183d8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-03 19:07:34 +0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations'  }
-  resources :messages do
-    resources :comments
-  end
-  root 'messages#index'
-end
